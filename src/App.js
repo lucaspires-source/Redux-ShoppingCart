13279698d838d15e81a877070e47a4c65ef200d3
lucaspires-source <pires.lucas94@gmail.com>
2021-04-19 15:41:30 -0300

    final project

--- src/App.js
+++ src/App.js
@@ -5,15 +5,29 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from "redux";
+
+import reducer from './redux/reducer'
+import { Provider } from "react-redux";
+//initial store 
+const initialStore ={
+  cart:cartItems,
+  total:12234,
+  amount:5
+}
+
+
+const store = createStore(reducer,initialStore);
+
 
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer  />
+    </Provider>
   );
 }
 
